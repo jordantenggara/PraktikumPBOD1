1338eadb957f69a5cbac930521231f075c1a763a
Jordan Tenggara <toflowdown@gmail.com>
2026-02-24 13:34:57 +0700

    Pertemuan 1 selesai

--- Pertemuan1/src/Mtitik.java
+++ Pertemuan1/src/Mtitik.java
@@ -2,7 +2,7 @@ public class Mtitik {
     public static void main(String[] args){
         Titik T1 = new Titik();
         T1.setAbsis(3);
-        T1.setOrdinat(4);
+        T1.setOrdinat(100);
         T1.PrintTitik();
         T1.geser(3,4);
         T1.PrintTitik();
