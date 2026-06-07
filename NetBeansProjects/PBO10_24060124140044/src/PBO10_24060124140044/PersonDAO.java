9af45adf844097dfbf784dc3d240a1fd77c3b3e0
Enggar <toflowdown@gmail.com>
2026-06-07 23:06:57 +0700

    Delete NetBeansProjects directory

--- NetBeansProjects/PBO10_24060124140044/src/PBO10_24060124140044/PersonDAO.java
+++
@@ -1,9 +0,0 @@
-package pbo10_24060124140044;
-
-/*
-   File      : PersonDAO.java
-   Deskripsi : interface untuk person access object
- */
-public interface PersonDAO {
-    public void savePerson(Person P) throws Exception;
-}
